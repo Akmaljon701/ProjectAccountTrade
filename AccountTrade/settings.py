f973cc1354f20452ed83743a4e5a60d44167adee
Akmaljon <akmalkhanever@gmail.com>
2023-09-22 16:13:23 +0500

    ignore env

--- AccountTrade/settings.py
+++ AccountTrade/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21+a$rsaq*!r7-fgayrj&ix!sj95#56r3g*6s7jln-8*49t(vg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
